cee1c7c08484ca5ebb1eae6227ede656991bbc50
Kirill Krinkin <krinkin@users.noreply.github.com>
2025-07-27 11:36:25 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -12,20 +12,19 @@ qemu-system-riscv64 -machine virt -bios none -kernel "$BINARY" -s -S -nographic 
 QEMU_PID=$!
 sleep 1
 
-# Генерируем GDB команды динамически
+# Generate commands
 GDB_COMMANDS="-ex \"file $BINARY\" -ex \"target remote localhost:1234\" -ex \"set architecture riscv:rv64\" -ex \"set pagination off\" -ex \"b *0x$ENTRY\" -ex \"continue\""
 
-# Добавляем начальное состояние
+# Initial state
 GDB_COMMANDS="$GDB_COMMANDS -ex \"printf \\\"\\\\n=== STEP 0 ===\\\\n\\\"\" -ex \"x/1i \\\$pc\" -ex \"info registers t0 t1 t2\""
 
-# Генерируем команды для каждого шага
+# Each step command
 for ((i=1; i<=STEPS; i++)); do
     GDB_COMMANDS="$GDB_COMMANDS -ex \"stepi\" -ex \"printf \\\"\\\\n=== STEP $i ===\\\\n\\\"\" -ex \"x/1i \\\$pc-4\" -ex \"info registers t0 t1 t2\""
 done
 
 GDB_COMMANDS="$GDB_COMMANDS -ex \"quit\""
 
-# Выполняем GDB
 eval "gdb-multiarch -batch $GDB_COMMANDS"
 
 kill $QEMU_PID 2>/dev/null
